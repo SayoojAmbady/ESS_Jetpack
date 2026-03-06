ad966aef39a99b3c2437b2ac30775d424c2f9bcf
SayoojAmbady <sayoojsunil2255@gmail.com>
2026-03-06 16:24:52 +0530

    Api Integration for login started

---
+++ app/src/main/java/com/example/ess_jetpack/model/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.example.ess_jetpack.model
+
+data class  LoginRequest(
+    val employeeCode: String,
+    val password: String,
+)
---
+++ app/src/main/java/com/example/ess_jetpack/model/LoginResponse.kt
@@ -0,0 +1,9 @@
+package com.example.ess_jetpack.model
+
+import android.os.Message
+
+class LoginResponse(
+    val status: Boolean,
+    val message: String,
+    val Token: String?
+)
